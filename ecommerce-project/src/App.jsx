68797d3e868db47bd9655301c4ea517ca974e5df
Gopalsamy26 <gopal@ausmit.in>
2026-03-21 15:19:31 +0530

    move the home page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,9 +1,12 @@
 import { HomePage } from './pages/HomePage'
+import { Routes, Route} from 'react-router'
 import './App.css'
 
 function App() {
   return (
-    <HomePage />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+    </Routes>
   )
 }
 
